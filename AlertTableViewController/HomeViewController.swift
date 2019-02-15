c9364ba0754d2f72076de1a3778e46a39280e3a4
Nayem <nayem@Nayems-MacBook-Pro.local>
2019-02-15 22:39:38 +0600

    Pass the generic element back when any item is selected from the picker options

--- AlertTableViewController/HomeViewController.swift
+++ AlertTableViewController/HomeViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    private var alertTableView: AlertTableView<Model>!
+    /**
+     Strong reference to picker object is a must because otherwise options in the picker aren't retained.
+     
+     Generic placeholder must be satisfied at the point of property declaration (in order to store a strong reference) because contextual information is passed through higher order function of initializer like:
+     ```
+     ... labels: ({ "\($0.name)'s ID: \($0.id)" }) ...
+     ```
+    */
+    private var popover: PopoverPicker<Model>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func popupButtonDidTap(_ sender: UIButton) {
+        
         let models = Model.allModels()
-        alertTableView = AlertTableView(values: models, labels: ({ "\($0.name)'s ID: \($0.id)" }), onSelect: { model in
-            print(model)
+        popover = PopoverPicker(values: models, labels: ({ "\($0.name)'s ID: \($0.id)" }), onSelect: { selectedModel in
+            print(selectedModel.name)
         })
-        alertTableView.present()
+        popover.present()
+        
     }
 
 }
--- AlertTableViewController/PopoverPicker.swift
+++ AlertTableViewController/PopoverPicker.swift
@@ -1,76 +1,68 @@
 //
-//  AlertTableViewController.swift
+//  PopoverPicker.swift
 //  AlertTableViewController
 //
-//  Created by Nayem BJIT on 2/14/19.
+//  Created by Nayem on 2/15/19.
 //  Copyright © 2019 BJIT Ltd. All rights reserved.
 //
 
 import UIKit
 import SDCAlertView
 
-class AlertTableView<Element>: UIView, UITableViewDataSource {
-
-//    private var schedules = [String]()
+class PopoverPicker<Element>: NSObject, UITableViewDataSource, UITableViewDelegate {
+    
     typealias SelectionHandler = (Element) -> Void
     typealias LabelProvider = (Element) -> String
     
-    private var values = [Element]()
-    private var labels: LabelProvider?
-    private var onSelect: SelectionHandler?
-
+    private let values : [Element]
+    private let labels : LabelProvider?
+    private let onSelect : SelectionHandler?
+    
     private var alert: AlertController!
     private let tableView = UITableView(frame: .zero, style: .plain)
     private let tableViewCellHeight: CGFloat = 135/3
-
-    convenience init(values : [Element], labels : @escaping LabelProvider = String.init(describing:), onSelect : SelectionHandler?) {
-        self.init(frame: .zero)
+    
+    init(values: [Element], labels: LabelProvider? = String.init(describing:), onSelect: SelectionHandler?) {
         self.values = values
-        self.labels = labels
         self.onSelect = onSelect
+        self.labels = labels
+        super.init()
         setupTableView()
         setupAlertController()
     }
-
-    override private init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
+    
     private func setupTableView() {
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         tableView.backgroundColor = .clear
         tableView.reloadData()
         tableView.rowHeight = tableViewCellHeight
         tableView.separatorInset = .zero
     }
-
+    
     private func setupAlertController() {
         let attributes = [NSAttributedString.Key.foregroundColor : UIColor.white, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16)]
         let attributedTitle = NSAttributedString(string: "Schedules", attributes: attributes)
         alert = AlertController(attributedTitle: attributedTitle, attributedMessage: nil)
         let dismissAction = AlertAction(title: "Dismiss", style: .normal)
         alert.addAction(dismissAction)
-
+        
         // Custom content view
         let separatorAboveTableView = UIView(frame: .zero)
         separatorAboveTableView.translatesAutoresizingMaskIntoConstraints = false
         alert.contentView.addSubview(separatorAboveTableView)
-
+        
         separatorAboveTableView.leadingAnchor.constraint(equalTo: alert.contentView.leadingAnchor).isActive = true
         separatorAboveTableView.trailingAnchor.constraint(equalTo: alert.contentView.trailingAnchor).isActive = true
         separatorAboveTableView.topAnchor.constraint(equalTo: alert.contentView.topAnchor).isActive = true
         separatorAboveTableView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
-
+        
         separatorAboveTableView.backgroundColor = .white
-
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         alert.contentView.addSubview(tableView)
-
+        
         let maximumNumberOfCellToBeVisible = 3
         let numberOfVisibleCell = values.count < maximumNumberOfCellToBeVisible ? values.count : maximumNumberOfCellToBeVisible
         let tableViewMaxHeight = tableViewCellHeight * CGFloat(numberOfVisibleCell) // needs logic
@@ -79,7 +71,7 @@ class AlertTableView<Element>: UIView, UITableViewDataSource {
         tableView.topAnchor.constraint(equalTo: separatorAboveTableView.bottomAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: alert.contentView.bottomAnchor).isActive = true
         tableView.heightAnchor.constraint(equalToConstant: tableViewMaxHeight).isActive = true
-
+        
         // Appearance
         alert.contentView.tintColor = .white
         let visualStyle = AlertVisualStyle(alertStyle: .alert)
@@ -91,16 +83,12 @@ class AlertTableView<Element>: UIView, UITableViewDataSource {
         visualStyle.contentPadding.left = 0
         visualStyle.contentPadding.right = 0
         alert.visualStyle = visualStyle
-
+        
         alert.behaviors = .dismissOnOutsideTap
     }
-
+    
     func present() {
         alert.present()
-    }
-
-    func dismiss() {
-        alert.dismiss()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -116,5 +104,13 @@ class AlertTableView<Element>: UIView, UITableViewDataSource {
         cell.backgroundColor = .clear
         return cell
     }
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        alert.dismiss { [weak self] in
+            guard let `self` = self else { return }
+            let value = self.values[indexPath.row]
+            self.onSelect?(value)
+        }
+    }
+    
 }
